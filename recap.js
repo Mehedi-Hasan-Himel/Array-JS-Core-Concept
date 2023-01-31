2bb915bf43448ecdf11b41f3da7aff3df09b3f10
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-31 12:10:20 +0600

    [ADD] recap

---
+++ recap.js
@@ -0,0 +1,45 @@
+// 1. Variable
+var myName = `Mehedi Hasan Himel`;
+
+// math operation   +, -, *, /, %, ++, --
+// shortcut for math operation  +=, -=, *=, /=, %=
+
+// 2. Array
+var friends = [`Abul`, `Babul`, `Kabul`, `Dabul`];
+var thirdFriend = friends[2];
+friends[3] = `Dambool`;
+
+// 3. Conditional , conditional symbols: ==, ===, !=, !==, >, <, >=, <=
+
+if (friends.length < 2) {
+  console.log(`fokir tor kono friend nai...`);
+} else {
+  console.log(`tor friend ache...`);
+}
+
+// 4. loops
+
+var number = 0; 
+while (number <= 6) {
+   console.log(number);
+   number++
+}
+
+for (let i = 0; i < array.length; i++) {
+   const element = array[i];
+   
+}
+
+// 5. function
+function isMoonUp(time) {
+   if(time>=19 && time<=5){
+      return true;
+   }
+}
+
+// 6. object
+var jantus = {
+   height: 5.5,
+   romanticicm: `Heavy`,
+   bapArTaka: `Borolox`,
+}
