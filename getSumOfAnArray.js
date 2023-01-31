e351a9794ea184fb9df806a9845818176cac7fca
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-31 13:31:32 +0600

    [ADD] getSumOfAnArray

---
+++ getSumOfAnArray.js
@@ -0,0 +1,15 @@
+function getSumOfAnArray(numbers) {
+   let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+     const index = i;
+     const element = numbers[i];
+     sum  = sum + element;
+   //   console.log(index, element, sum);
+  }
+   return sum;
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 45, 91,233,];
+const result  = getSumOfAnArray(myNumbers);
+
+console.log(result);
